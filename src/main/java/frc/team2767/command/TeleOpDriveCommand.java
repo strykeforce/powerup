c69f98b167a45cef001d4050d9a9c1ae6bffadef
Roberts Kalnins <rkalnins314@gmail.com>
2018-01-24 21:08:12 -0500

    Add rate limits and expo scale

--- src/main/java/frc/team2767/command/TeleOpDriveCommand.java
+++ src/main/java/frc/team2767/command/TeleOpDriveCommand.java
@@ -3,22 +3,36 @@ package frc.team2767.command;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.team2767.Controls;
 import frc.team2767.Robot;
+import frc.team2767.inputadjustment.ExpoScale;
+import frc.team2767.inputadjustment.RateLimit;
 import frc.team2767.subsystem.DriveSubsystem;
 import frc.team2767.subsystem.DriveSubsystem.DriveMode;
+
+import java.util.logging.Logger;
 
 public final class TeleOpDriveCommand extends Command {
 
   private final DriveSubsystem drive;
   private final Controls controls;
+  private ExpoScale expoScale;
+  private final RateLimit rateLimit;
 
   public TeleOpDriveCommand() {
     drive = Robot.COMPONENT.driveSubsystem();
     controls = Robot.COMPONENT.controls();
+    rateLimit  = new RateLimit(0.4);
+    expoScale = new ExpoScale(0.05, 1.0);
     requires(drive);
   }
 
   private static double applyDeadband(double input) {
     return Math.abs(input) > 0.05 ? input : 0;
+  }
+
+  private double applyInputAdjustments(double input) {
+//    return rateLimit.applyRateLimit(expoScale.applyExpoScale(input));
+    return expoScale.applyExpoScale(input);
+//    return rateLimit.applyRateLimit(input);
   }
 
   @Override
@@ -28,9 +42,13 @@ public final class TeleOpDriveCommand extends Command {
 
   @Override
   protected void execute() {
-    double forward = applyDeadband(controls.getForward());
-    double strafe = applyDeadband(controls.getStrafe());
-    double azimuth = applyDeadband(controls.getAzimuth());
+//    double forward = applyDeadband(controls.getForward());
+//    double strafe = applyDeadband(controls.getStrafe());
+//    double azimuth = applyDeadband(controls.getAzimuth());
+
+    double forward = applyInputAdjustments(controls.getForward());
+    double strafe = applyInputAdjustments(controls.getStrafe());
+    double azimuth = applyInputAdjustments(controls.getAzimuth());
 
     drive.drive(forward, strafe, azimuth);
   }
---
+++ src/main/java/frc/team2767/inputadjustment/RateLimit.java
@@ -0,0 +1,24 @@
+package frc.team2767.inputadjustment;
+
+public class RateLimit {
+
+  private static double rateLimit;
+  private static double lastLimit = 0;
+
+  public RateLimit(double rateLimit) {
+    RateLimit.rateLimit = rateLimit;
+  }
+
+  public double applyRateLimit(double input) {
+    double y;
+    if (Math.abs(input - lastLimit) > rateLimit) {
+      y = lastLimit + Math.copySign(rateLimit, input - rateLimit);
+    }
+    else {
+      y = input;
+    }
+
+    lastLimit = y;
+    return y;
+  }
+}
