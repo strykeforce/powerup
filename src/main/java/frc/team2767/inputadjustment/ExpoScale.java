a76c6b4b25e7700bdbc122440f212bb962ab5636
Roberts Kalnins <rkalnins314@gmail.com>
2018-01-24 21:08:12 -0500

    Add rate limits and expo scale

---
+++ src/main/java/frc/team2767/inputadjustment/ExpoScale.java
@@ -0,0 +1,24 @@
+package frc.team2767.inputadjustment;
+
+/**
+ * Applies exponential scaling and deadband to joystick inputs
+ */
+public class ExpoScale {
+
+  private static double deadband;
+  private static double scale; ;
+
+  public ExpoScale(double deadband, double scale) {
+    ExpoScale.deadband = deadband;
+    ExpoScale.scale = scale;
+  }
+
+  private static final double offset = 1.0 / (scale * Math.pow(1 - deadband, 3) + (1 - scale) * (1 - deadband));
+
+  public static double applyExpoScale(double input) {
+    double y;
+
+    y = input > 0 ? input + deadband : input - deadband;
+    return (scale * Math.pow(y, 3) + (1 - scale) * y) * offset;
+  }
+}
