e1eb971fe0be947b11548bd623a4bd3c660ecc9c
Roberts Kalnins <rkalnins314@gmail.com>
2018-01-27 13:42:36 -0500

    FIx code style

--- src/main/java/frc/team2767/inputadjustment/ExpoScale.java
+++ src/main/java/frc/team2767/inputadjustment/ExpoScale.java
@@ -1,8 +1,6 @@
 package frc.team2767.inputadjustment;
 
-/**
- * Applies exponential scaling and deadband to joystick inputs
- */
+/** Applies exponential scaling and deadband to joystick inputs */
 public class ExpoScale {
 
   private final double deadband;
@@ -13,7 +11,6 @@ public class ExpoScale {
     this.deadband = deadband;
     this.scale = scale;
     offset = 1.0 / (scale * Math.pow(1 - deadband, 3) + (1 - scale) * (1 - deadband));
-
   }
 
   public double applyExpoScale(double input) {
