b437544b587dd57c6d4fd645361285c9f218c448
Roberts Kalnins <rkalnins314@gmail.com>
2018-01-27 21:03:38 -0500

    Update expo-rate scale and limit

--- src/main/java/frc/team2767/command/TeleOpDriveCommand.java
+++ src/main/java/frc/team2767/command/TeleOpDriveCommand.java
@@ -7,9 +7,12 @@ import frc.team2767.inputadjustment.ExpoScale;
 import frc.team2767.inputadjustment.RateLimit;
 import frc.team2767.subsystem.DriveSubsystem;
 import frc.team2767.subsystem.DriveSubsystem.DriveMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class TeleOpDriveCommand extends Command {
 
+  private static final Logger logger = LoggerFactory.getLogger(DriveSubsystem.class);
   private final DriveSubsystem drive;
   private final Controls controls;
   private ExpoScale expoScale;
@@ -18,8 +21,8 @@ public final class TeleOpDriveCommand extends Command {
   public TeleOpDriveCommand() {
     drive = Robot.COMPONENT.driveSubsystem();
     controls = Robot.COMPONENT.controls();
-    rateLimit = new RateLimit(0.4, 0.0);
-    expoScale = new ExpoScale(0.05, 1.0);
+    rateLimit = new RateLimit(0.3, 0.0);
+    expoScale = new ExpoScale(0.05, 0);
     requires(drive);
   }
 
@@ -28,6 +31,12 @@ public final class TeleOpDriveCommand extends Command {
   }
 
   private double applyInputAdjustments(double input) {
+    if (rateLimit.applyRateLimit(expoScale.applyExpoScale(input)) != 0.0) {
+      logger.debug(
+          "Premath={} Postmath={}",
+          input,
+          rateLimit.applyRateLimit(expoScale.applyExpoScale(input)));
+    }
     return rateLimit.applyRateLimit(expoScale.applyExpoScale(input));
   }
 
@@ -38,13 +47,13 @@ public final class TeleOpDriveCommand extends Command {
 
   @Override
   protected void execute() {
-    //    double forward = applyDeadband(controls.getForward());
-    //    double strafe = applyDeadband(controls.getStrafe());
-    //    double azimuth = applyDeadband(controls.getAzimuth());
+    double forward = applyDeadband(controls.getForward());
+    double strafe = applyDeadband(controls.getStrafe());
+    double azimuth = applyDeadband(controls.getAzimuth());
 
-    double forward = applyInputAdjustments(controls.getForward());
-    double strafe = applyInputAdjustments(controls.getStrafe());
-    double azimuth = applyInputAdjustments(controls.getAzimuth());
+    //    double forward = applyInputAdjustments(controls.getForward());
+    //    double strafe = applyInputAdjustments(controls.getStrafe());
+    //    double azimuth = controls.getAzimuth();
 
     drive.drive(forward, strafe, azimuth);
   }
