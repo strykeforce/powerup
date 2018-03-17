2c36bdef004776b84d7b8c0077d47ea54c72fc07
Jeff Hutchison <jeff@jeffhutchison.com>
2018-03-17 09:25:39 -0400

    Trigger command after distance travelled

--- src/main/java/frc/team2767/command/auton/ScaleCommandGroup.java
+++ src/main/java/frc/team2767/command/auton/ScaleCommandGroup.java
@@ -1,53 +1,65 @@
 package frc.team2767.command.auton;
 
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.command.WaitCommand;
 import frc.team2767.command.StartPosition;
 import frc.team2767.command.intake.IntakeEject;
 import frc.team2767.command.lift.LiftPosition;
 import frc.team2767.command.shoulder.ShoulderPosition;
+import frc.team2767.motion.PathController;
 import frc.team2767.subsystem.IntakeSubsystem;
 
 public class ScaleCommandGroup extends PowerUpCommandGroup {
 
   public ScaleCommandGroup(Side side) {
     super();
-    addParallel(new LiftUp(side));
-    addSequential(new PathCommand(side.path, side.startPosition));
-
-    addSequential(new AzimuthShoulderMoveCommandGroup(side));
+    addParallel(new ShoulderPosition(ShoulderPosition.Position.TIGHT_STOW));
+    PathCommand pathCommand = new PathCommand(side.path, side.startPosition);
+    addParallel(new PositionForCubeLaunch(pathCommand.getPathController(), side));
+    addSequential(pathCommand);
+    addSequential(new AzimuthCommand(side.azimuth));
     addSequential(new IntakeEject(IntakeSubsystem.Mode.FAST_EJECT));
-    //    addSequential(new Stow());
   }
 
   public enum Side {
-    LEFT("left_scale", -42.0, StartPosition.LEFT),
-    RIGHT("right_scale", 40.0, StartPosition.RIGHT),
+    LEFT("left_scale", -42.0, 500_000, StartPosition.LEFT),
+    RIGHT("right_scale", 15.0, 500_000, StartPosition.RIGHT),
     ;
 
     private final String path;
     private final double azimuth;
+    private final int distance;
     private final StartPosition startPosition;
 
-    Side(String path, double azimuth, StartPosition startPosition) {
+    Side(String path, double azimuth, int distance, StartPosition startPosition) {
       this.path = path;
       this.azimuth = azimuth;
+      this.distance = distance;
       this.startPosition = startPosition;
     }
   }
 
-  private static class AzimuthShoulderMoveCommandGroup extends CommandGroup {
+  private static class PositionForCubeLaunch extends CommandGroup {
 
-    private AzimuthShoulderMoveCommandGroup(Side side) {
-      addParallel(new AzimuthCommand(side.azimuth));
-      addParallel(new ShoulderPosition(ShoulderPosition.Position.LAUNCH_SCALE));
+    private PositionForCubeLaunch(PathController pathController, Side side) {
+      addSequential(new WaitForDistance(pathController, side.distance));
+      addSequential(new LiftPosition(LiftPosition.Position.SCALE_HIGH));
+      addSequential(new ShoulderPosition(ShoulderPosition.Position.LAUNCH_SCALE));
     }
   }
 
-  private static class LiftUp extends CommandGroup {
-    private LiftUp(Side side) {
-      addSequential(new WaitCommand(0.5));
-      addSequential(new LiftPosition(LiftPosition.Position.SCALE_HIGH));
+  private static class WaitForDistance extends Command {
+    private final PathController pathController;
+    private final int distance;
+
+    public WaitForDistance(PathController pathController, int distance) {
+      this.pathController = pathController;
+      this.distance = distance;
+    }
+
+    @Override
+    protected boolean isFinished() {
+      return pathController.getDistance() > distance;
     }
   }
 }
