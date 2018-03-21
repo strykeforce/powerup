1d0f9798d361516611ca31dd9a495a8090d8f17e
Roberts Kalnins <rkalnins314@gmail.com>
2018-03-20 21:02:15 -0400

    Lidar fixes

--- src/main/java/frc/team2767/subsystem/LidarSubsystem.java
+++ src/main/java/frc/team2767/subsystem/LidarSubsystem.java
@@ -5,10 +5,11 @@ import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.filters.LinearDigitalFilter;
-import javax.inject.Inject;
-import javax.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
 
 @Singleton
 public class LidarSubsystem extends Subsystem implements PIDSource {
