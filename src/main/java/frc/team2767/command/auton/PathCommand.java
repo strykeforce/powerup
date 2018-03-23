8c472f52d743e96187597e3314013e230380bbe9
Jeff Hutchison <jeff@jeffhutchison.com>
2018-03-23 13:14:52 -0400

    Move logging to path controller

--- src/main/java/frc/team2767/command/auton/PathCommand.java
+++ src/main/java/frc/team2767/command/auton/PathCommand.java
@@ -5,59 +5,48 @@ import frc.team2767.Robot;
 import frc.team2767.command.StartPosition;
 import frc.team2767.motion.PathController;
 import frc.team2767.subsystem.DriveSubsystem;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class PathCommand extends Command {
 
   private final DriveSubsystem driveSubsystem = Robot.INJECTOR.driveSubsystem();
   private final PathController path;
-  private static final Logger logger = LoggerFactory.getLogger(PathCommand.class);
 
-  private String pathName;
-
-  public PathCommand(String name, String pathName, StartPosition startPosition) {
+  public PathCommand(String name, String pathName, double azimuth) {
     super(name);
-
-    this.pathName = pathName;
-
-    path = Robot.INJECTOR.pathControllerFactory().create(pathName);
-    switch (startPosition) {
-      case UNKNOWN:
-      case CENTER:
-        path.setTargetAzimuth(0);
-        logger.debug("target azm = {}", 0.0);
-        break;
-      case LEFT:
-        path.setTargetAzimuth(90.0);
-        logger.debug("target azm = {}", 90.0);
-        break;
-      case RIGHT:
-        path.setTargetAzimuth(-90.0);
-        logger.debug("target azm = {}", -90.0);
-        break;
-    }
-    requires(driveSubsystem);
-  }
-
-  // TODO: Look at designated constructor
-  public PathCommand(String pathName, double azimuth) {
     path = Robot.INJECTOR.pathControllerFactory().create(pathName);
     path.setTargetAzimuth(azimuth);
     requires(driveSubsystem);
   }
 
+  public PathCommand(String pathName, double azimuth) {
+    this("PathCommand", pathName, azimuth);
+  }
+
+  public PathCommand(String name, String pathName, StartPosition startPosition) {
+    this(name, pathName, targetAzimuth(startPosition));
+  }
+
   public PathCommand(String path, StartPosition startPosition) {
-    this("Path", path, startPosition);
+    this("PathCommand", path, startPosition);
   }
 
   public PathCommand(String path) {
     this(path, StartPosition.UNKNOWN);
   }
 
+  private static double targetAzimuth(StartPosition startPosition) {
+    switch (startPosition) {
+      case LEFT:
+        return 90d;
+      case RIGHT:
+        return -90d;
+      default:
+        return 0d;
+    }
+  }
+
   @Override
   protected void initialize() {
-    logger.debug("starting {}", pathName);
     driveSubsystem.drivePath(path);
   }
 
@@ -68,7 +57,6 @@ public class PathCommand extends Command {
 
   @Override
   protected void end() {
-    logger.debug("end");
     driveSubsystem.endPath();
   }
 
