a8cf9218accd820ef6e4f555ecd7346f39786366
Jeff Hutchison <jeff@jeffhutchison.com>
2018-03-26 16:22:31 -0400

    Block out switch auton commands

--- src/main/java/frc/team2767/command/auton/SwitchSameCube1Deliver.java
+++ src/main/java/frc/team2767/command/auton/SwitchSameCube1Deliver.java
@@ -1,7 +1,24 @@
 package frc.team2767.command.auton;
 
-import edu.wpi.first.wpilibj.command.InstantCommand;
+import com.moandjiezana.toml.Toml;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.team2767.Robot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class SwitchSameCube1Deliver extends InstantCommand {
-  public SwitchSameCube1Deliver(StartPosition startPosition) {}
+public class SwitchSameCube1Deliver extends CommandGroup {
+
+  private final int kDistance;
+  private final String settings;
+
+  private static final Logger logger = LoggerFactory.getLogger(SwitchSameCube1Deliver.class);
+
+  public SwitchSameCube1Deliver(StartPosition startPosition) {
+    settings = startPosition == StartPosition.RIGHT ? "R_SW_S_C1D" : "L_SW_S_C1D";
+    Toml toml = Robot.INJECTOR.settings().getAutonSettings(settings);
+    String path = toml.getString("path");
+    kDistance = toml.getLong("distance").intValue();
+
+    PathCommand pathCommand = new PathCommand(path, startPosition);
+  }
 }
