b368fe8d17a23342df79a2c83a7a684d64d7b3eb
Roberts Kalnins <rkalnins314@gmail.com>
2018-03-28 09:09:44 -0400

    Feature/tune autons (#83)
    * Add field dimension documentation
    
    * Add field dimension documentation
    
    * Add field dimension documentation
    
    * Change auton commands
    
    * Adjust auton settings
    
    * Adjust auton settings
    
    * Update scale auton
    
    * Update intake subsystem
    
    * Add drive-by cubing to right scale
    
    * Tune auton paths
    
    * Continue tuning autons
    
    * Undo changes
    
    * Update soft limit

--- src/main/java/frc/team2767/command/auton/SwitchSameCube1Deliver.java
+++ src/main/java/frc/team2767/command/auton/SwitchSameCube1Deliver.java
@@ -3,11 +3,15 @@ package frc.team2767.command.auton;
 import com.moandjiezana.toml.Toml;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.team2767.Robot;
+import frc.team2767.command.intake.IntakeEject;
+import frc.team2767.command.shoulder.ShoulderPosition;
+import frc.team2767.subsystem.IntakeSubsystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SwitchSameCube1Deliver extends CommandGroup {
   private static final double START_POSITION_YAW = 0d;
+  private static final double EJECT_DURATION = 0.5;
 
   private static final Logger logger = LoggerFactory.getLogger(SwitchSameCube1Deliver.class);
   private final int kDistance;
@@ -20,6 +24,37 @@ public class SwitchSameCube1Deliver extends CommandGroup {
     kDistance = toml.getLong("distance").intValue();
 
     PathCommand pathCommand = new PathCommand(path, startPosition.getPathAngle(START_POSITION_YAW));
+
+    addParallel(new ShoulderPosition(ShoulderPosition.Position.TIGHT_STOW));
+
+    addParallel(
+        new CommandGroup() {
+          {
+            addSequential(new WaitForDistance(pathCommand.getPathController(), kDistance));
+
+            addSequential(
+                new CommandGroup() {
+                  {
+                    addParallel(new ShoulderPosition(ShoulderPosition.Position.LAUNCH_SWITCH));
+                  }
+
+                  @Override
+                  protected void end() {
+                    logger.trace("LiftPosition || ShoulderPosition ENDED");
+                  }
+                });
+
+            addSequential(new IntakeEject(IntakeSubsystem.Mode.SWITCH_EJECT, EJECT_DURATION));
+          }
+
+          @Override
+          protected void end() {
+            logger.trace(
+                "WaitForDistance → (LiftPosition || ShoulderPosition) → IntakeEject ENDED");
+          }
+        });
+
+    addSequential(pathCommand);
   }
 
   @Override
