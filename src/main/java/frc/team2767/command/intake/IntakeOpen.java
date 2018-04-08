6e1520f760344d88b7006f5f9cc0e9011680df12
mwitcpalek <35316453+mwitcpalek@users.noreply.github.com>
2018-04-08 15:05:05 -0400

    Add command to sequence to drop cube (#99)

---
+++ src/main/java/frc/team2767/command/intake/IntakeOpen.java
@@ -0,0 +1,19 @@
+package frc.team2767.command.intake;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
+import frc.team2767.Robot;
+import frc.team2767.subsystem.IntakeSubsystem;
+
+public class IntakeOpen extends InstantCommand{
+    private final IntakeSubsystem intakeSubsystem = Robot.INJECTOR.intakeSubsystem();
+
+    public IntakeOpen() {
+        super("IntakeOpen");
+        requires(intakeSubsystem);
+    }
+
+    @Override
+    protected void initialize() {
+        intakeSubsystem.run(IntakeSubsystem.Mode.OPEN);
+    }
+}
