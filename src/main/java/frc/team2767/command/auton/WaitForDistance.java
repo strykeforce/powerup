ae125aec195a5060344b4dc6a7dbc713e6d994bd
rabsamu <30671368+rabsamu@users.noreply.github.com>
2018-04-20 19:18:05 -0400

    Get rid of the FIXME's in Cube1Deliver

--- src/main/java/frc/team2767/command/auton/WaitForDistance.java
+++ src/main/java/frc/team2767/command/auton/WaitForDistance.java
@@ -7,14 +7,16 @@ public class WaitForDistance extends Command {
 
   private final PathController pathController;
   private final int distance;
+  private int goalDistance;
 
   public WaitForDistance(PathController pathController, int distance) {
     this.pathController = pathController;
     this.distance = distance;
+    goalDistance = (int) Math.round(pathController.getTicks())-distance;
   }
 
   @Override
   protected boolean isFinished() {
-    return pathController.getDistance() > distance;
+    return pathController.getDistance() > goalDistance;
   }
 }
