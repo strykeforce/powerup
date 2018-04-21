2eed4048e89c64626d956a1ab5944cf0f5260ce5
Jeff Hutchison <jeff@jeffhutchison.com>
2018-04-21 10:09:09 -0400

    Use dedicated azimuth PID

--- src/main/java/frc/team2767/motion/MotionController.java
+++ src/main/java/frc/team2767/motion/MotionController.java
@@ -3,6 +3,7 @@ package frc.team2767.motion;
 import com.google.auto.factory.AutoFactory;
 import com.google.auto.factory.Provided;
 import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.PIDController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.strykeforce.thirdcoast.swerve.SwerveDrive;
@@ -16,11 +17,15 @@ public class MotionController {
   private static final int T2_MS = 100;
   private static final double V_PROG = 12_000 * 10; // ticks/sec
 
+  private static final double K_P = 0.01;
+  private static final double OUTPUT_RANGE = 0.25;
+  private static final double ABS_TOL = 1.0;
+
   private final double forwardComponent;
   private final double strafeComponent;
   private final SwerveDrive drive;
   private final MotionProfile motionProfile;
-  private final AzimuthController azimuthController;
+  private final PIDController pidController;
   private final Notifier notifier;
   private double azimuth;
 
@@ -38,30 +43,35 @@ public class MotionController {
     strafeComponent = Math.sin(Math.toRadians(direction)) / ticksPerSecMax;
 
     motionProfile = new MotionProfile(DT_MS, T1_MS, T2_MS, V_PROG, distance);
-    azimuthController = azimuthControllerFactory.create(this::updateAzimuth);
-    azimuthController.setSetpoint(azimuth);
+    pidController = new PIDController(K_P, 0, 0, drive.getGyro(), this::updateAzimuth, 0.01);
+    pidController.setSetpoint(azimuth);
+    pidController.setInputRange(-180d, 180d);
+    pidController.setOutputRange(-OUTPUT_RANGE, OUTPUT_RANGE);
+    pidController.setContinuous(true);
+    pidController.setAbsoluteTolerance(ABS_TOL);
+
     notifier = new Notifier(this::updateDrive);
   }
 
   public void start() {
     logger.info("START motion gyro angle = {}", drive.getGyro().getAngle());
     notifier.startPeriodic(DT_MS / 1000.0);
-    azimuthController.enable();
+    pidController.enable();
   }
 
   public void stop() {
     logger.info("FINISH motion");
     drive.drive(0, 0, 0);
     notifier.stop();
-    azimuthController.disable();
+    pidController.disable();
   }
 
   public boolean isFinished() {
-    return motionProfile.isFinished() && azimuthController.onTarget();
+    return motionProfile.isFinished() && pidController.onTarget();
   }
 
   private synchronized void updateAzimuth(double azimuth) {
-    this.azimuth = azimuthController.onTarget() ? 0 : azimuth;
+    this.azimuth = pidController.onTarget() ? 0 : azimuth;
   }
 
   private void updateDrive() {
